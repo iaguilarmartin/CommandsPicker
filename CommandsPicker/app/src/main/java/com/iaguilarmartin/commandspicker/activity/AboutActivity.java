cdef304c72b7db7ab27063941ff07aeb56d4818c
Ivan Aguilar Martin <iaguilarmartin@outlook.com>
2016-12-22 19:19:40 +0100

    AboutActivity added

---
+++ CommandsPicker/app/src/main/java/com/iaguilarmartin/commandspicker/activity/AboutActivity.java
@@ -0,0 +1,42 @@
+package com.iaguilarmartin.commandspicker.activity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.iaguilarmartin.commandspicker.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        setTitle(getString(R.string.about_menu_option));
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] addresses = {getString(R.string.config_contact_email)};
+                String subject = getString(R.string.contact_mail_subject);
+
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+                intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+                intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+}
--- CommandsPicker/app/src/main/java/com/iaguilarmartin/commandspicker/fragment/TablesFragment.java
+++ CommandsPicker/app/src/main/java/com/iaguilarmartin/commandspicker/fragment/TablesFragment.java
@@ -16,6 +16,7 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.iaguilarmartin.commandspicker.R;
+import com.iaguilarmartin.commandspicker.activity.AboutActivity;
 import com.iaguilarmartin.commandspicker.activity.SettingsActivity;
 import com.iaguilarmartin.commandspicker.adapter.TablesAdapter;
 import com.iaguilarmartin.commandspicker.model.CommanderApplication;
@@ -76,6 +77,11 @@ public class TablesFragment extends Fragment {
             case R.id.settings_option: {
                 // Displaying SettingsActivity in order to change number of tables
                 Intent intent = new Intent(getActivity(), SettingsActivity.class);
+                startActivity(intent);
+            } break;
+            case R.id.about_option: {
+                // Displaying AboutActivity
+                Intent intent = new Intent(getActivity(), AboutActivity.class);
                 startActivity(intent);
             } break;
         }
